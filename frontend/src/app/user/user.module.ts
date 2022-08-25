b31bdca8ae5ed2d93f5fe2ac0fdfe599d9e48c6c
Subhankar Roy <subhankarroylive@gmail.com>
2022-08-25 10:22:00 +0530

    set base url dashboard

--- frontend/src/app/user/user.module.ts
+++ frontend/src/app/user/user.module.ts
@@ -31,6 +31,11 @@ const routes = [
     component: ChangePasswordComponent,
     canActivate: [AuthGuard]
   },
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
 
 ]
 
